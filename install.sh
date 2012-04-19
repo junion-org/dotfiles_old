9610edf9089c2e5ab235470aed5e891baf84c3da
Jun ITO <jun@jAir.local>
2012-04-20 01:50:32 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # ファイルが存在していた場合，dotfiles_oldにすべて移動する
 mkdir ~/dotfiles_old
-mv ~/.vim dotfiles_old/
-mv ~/.vimrc* dotfiles_old/
+mv ~/.vim ~/dotfiles_old/
+mv ~/.vimrc* ~/dotfiles_old/
 
 # シンボリックリンクをはる
 ln -s ~/dotfiles/.vim ~/.vim
