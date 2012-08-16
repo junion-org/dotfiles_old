ab67f13c1acb76cb014fe3e495ef0fd0598af304
Jun ITO <jun@behemoth.local>
2012-08-17 01:38:55 +0900

    tmuxとか

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-# ファイルが存在していた場合，dotfiles_oldにすべて移動する
-mkdir ~/dotfiles.old
-mv ~/.vim ~/dotfiles.old/
-mv ~/.vimrc* ~/dotfiles.old/
 
 # シンボリックリンクをはる
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 # サブモジュールのインストール
 git submodule init
