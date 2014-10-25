c78a4c79c05463db81a3c54dbd33acd2d7a0fdea
junion <junion@junion.org>
2014-10-25 21:33:41 +0900

    change install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,17 @@
 # 既存dotfilesの削除とシンボリックリンク
 [ -f $HOME/dotfiles/relink.sh ] && source $HOME/dotfiles/relink.sh
 
-# NeoBundleのクローン
+# NeoBundleとvimprocのクローン
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-
-# vimの起動
-vim -c NeoBundleInstall!
+git clone https://github.com/Shougo/vimproc.vim ~/.vim/bundle/vimproc.vim
 
 # vimprocのインストール
-cd .vim/bundle/vimproc.vim
+cd ~/.vim/bundle/vimproc.vim
 make
-
-# ホームに戻って終了
 cd ~
+
+# vimの起動とプラグインのインストール
+vim -c NeoBundleInstall!
+
+# 終了
 echo 'done!'
