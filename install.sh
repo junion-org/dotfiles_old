ba5155d2ed7522b1e2607852fd648cfb6550c94a
junion <junion@junion.org>
2014-10-28 01:39:56 +0900

    change vimproc

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@
 
 # NeoBundleとvimprocのクローン
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-git clone https://github.com/Shougo/vimproc.vim ~/.vim/bundle/vimproc.vim
+#git clone https://github.com/Shougo/vimproc.vim ~/.vim/bundle/vimproc.vim
 
 # vimprocのインストール
-cd ~/.vim/bundle/vimproc.vim
-make
-cd ~
+#cd ~/.vim/bundle/vimproc.vim
+#make
+#cd ~
 
 # vimの起動とプラグインのインストール
 vim -c NeoBundleInstall!
